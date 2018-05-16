d225efbd57a08cbf0c4e660208dc433b4ca53c9a
Elijah <in3rd@yahoo.com>
2018-05-15 22:18:12 -0700

    Created basic api wit api response

--- src/main/java/lambda/ActivityServiceHandler.java
+++ src/main/java/lambda/ActivityServiceHandler.java
@@ -1,9 +1,32 @@
 package lambda;
 
-public class ActivityServiceHandler {
-    public void handleRequest()
-            throws Exception {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import org.json.simple.JSONObject;
+
+public class ActivityServiceHandler {
+    public void handleRequest( InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+
+        LambdaLogger logger = context.getLogger();
+
+        JSONObject responseJson = new JSONObject();
+
+        String responseCode = "200";
+        JSONObject headerJson = new JSONObject();
+        headerJson.put("x-custom-header", "my custom header value");
+
+        responseJson.put("isBase64Encoded", false);
+        responseJson.put("statusCode", responseCode);
+        responseJson.put("headers", headerJson);
+        responseJson.put("body", "YO I'M THE BDOY" );
+        logger.log(responseJson.toJSONString());
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
+        writer.write(responseJson.toJSONString());
+        writer.close();
     }
 }
