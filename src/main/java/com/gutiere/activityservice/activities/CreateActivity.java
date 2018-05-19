5bc6f932422b86d1fa21dcebc063127e1fb58781
Elijah <in3rd@yahoo.com>
2018-05-18 21:03:02 -0700

    Deploys a ddb table and can putItem into it! Wooohooo

--- src/main/java/com/gutiere/activityservice/activities/CreateActivity.java
+++ src/main/java/com/gutiere/activityservice/activities/CreateActivity.java
@@ -1,9 +1,30 @@
 package com.gutiere.activityservice.activities;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.gutiere.activityservice.data.Response;
+
+import java.util.UUID;
 
 public class CreateActivity {
     public Response handleRequest( String body ) {
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
+
+        DynamoDB dynamoDB = new DynamoDB( client );
+
+        Table activityTable = dynamoDB.getTable( "ActivityTable" );
+
+        Item item = new Item()
+                .with( "ActivityId", UUID.randomUUID().toString() )
+                .with( "Name", "Sleeping" )
+                .with( "Description", "Relaxing, healing, dreaming..." );
+
+        activityTable.putItem( item );
+
         return new Response( 200, "You've hit create activity." );
     }
 }
